44405d95a04b4a7cd30831e17bf698d54bc1a16e
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2015-06-04 23:22:17 +0900

    initial commit.

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'automake'
+maintainer       'Rudolph Miller'
+maintainer_email 'chopsticks.tk.ppfm@gmail.com'
+license          'MIT'
+description      'Installs/Configures automake'
+long_description 'Installs/Configures automake'
+version          '0.1.0'
