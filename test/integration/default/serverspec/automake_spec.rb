be5f946182d5dc6469706ed49b4ddef36faca4ce
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2015-06-08 03:57:20 +0900

    s/lacal/local.

--- test/integration/default/serverspec/automake_spec.rb
+++ test/integration/default/serverspec/automake_spec.rb
@@ -2,7 +2,7 @@ require 'serverspec'
 
 set :backend, :exec
 
-describe command("/usr/lacal/bin/automake --version") do
+describe command("/usr/local/bin/automake --version") do
   its (:stdout) { should match /automake/ }
 
   its (:stdout) { should match /1.11/ }
